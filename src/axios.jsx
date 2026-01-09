f1cafce9cbe9550afa1d456282960f678c5b2a6e
Chirayu Khalwa <22amtics493@gmail.com>
2026-01-09 20:49:14 +0530

    Ready for Vercel deployment

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
+  // 👇 UPDATED: Pointing to your live Render Backend
   baseURL: "http://localhost:8080/api",
 });
+
 delete API.defaults.headers.common["Authorization"];
 export default API;
