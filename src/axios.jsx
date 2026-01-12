ae3b508ecc4892d9bb66fabd19d6678ab3b5b5c7
Chirayu Khalwa <22amtics493@gmail.com>
2026-01-12 18:10:58 +0530

    Fix Navbar localhost error2

--- src/axios.jsx
+++ src/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // 👇 UPDATED: Pointing to your live Render Backend
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://ray-ecommerce.onrender.com/api",
 });
 
 delete API.defaults.headers.common["Authorization"];
